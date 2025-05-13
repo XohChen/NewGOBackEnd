90d08f0ee95ece1f2efdea21b866ebef733c7348
XohChen <xohchen@gmail.com>
2025-05-13 12:53:41 +0330

    cmd and api folder created v 0.001

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
 
+	"github.com/XohChen/NewGOBackEnd/service/user"
 	"github.com/gorilla/mux"
 )
 
@@ -20,5 +23,13 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
-	subrouter := router.PathPrefix
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+
 }
