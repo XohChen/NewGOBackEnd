a84cc3c2e80308b81bd6b5a22a81f86e6aaf04a5
XohChen <xohchen@gmail.com>
2025-05-15 12:29:18 -0700

    commit date : 2025-05-15 - 2

--- types/types.go
+++ types/types.go
@@ -6,3 +6,12 @@ type RegisterUserPayload struct {
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 }
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"createdAt"`
+}
