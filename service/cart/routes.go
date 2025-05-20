e0969154641807aa3c615d9e4e378ed1fa92a6b9
XohChen <xohchen@gmail.com>
2025-05-20 02:29:22 -0700

    complate jwt and handlers

--- service/cart/routes.go
+++ service/cart/routes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/XohChen/NewGOBackEnd/service/user/auth"
 	"github.com/XohChen/NewGOBackEnd/types"
 	"github.com/XohChen/NewGOBackEnd/utils"
 	"github.com/go-playground/validator/v10"
@@ -13,18 +14,20 @@ import (
 type Handler struct {
 	store        types.OrderStore
 	productStore types.ProductStore
+	userStore    types.UserStore
 }
 
-func NewHandler(store types.OrderStore, productStore types.ProductStore) *Handler {
+func NewHandler(store types.OrderStore, productStore types.ProductStore, userStore types.UserStore) *Handler {
 	return &Handler{store: store, productStore: productStore}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-	router.HandleFunc("/cart/checkout", h.handleCheckOut).Methods(http.MethodPost)
+	router.HandleFunc("/cart/checkout", auth.WithJWTAuth(h.handleCheckOut, h.userStore)).Methods(http.MethodPost)
 }
 
 func (h *Handler) handleCheckOut(w http.ResponseWriter, r *http.Request) {
-	userID := 0
+	userID := auth.GetUserIDFromContext(r.Context())
+
 	var cart types.CartCheckOutPayload
 	if err := utils.ParseJSON(r, &cart); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
--- service/user/auth/jwt.go
+++ service/user/auth/jwt.go
@@ -1,12 +1,22 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/XohChen/NewGOBackEnd/config"
+	"github.com/XohChen/NewGOBackEnd/types"
+	"github.com/XohChen/NewGOBackEnd/utils"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+type contextKey string
+
+const UserKey contextKey = "userID"
 
 func CreateJWT(secret []byte, userID int) (string, error) {
 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
@@ -22,4 +32,72 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 	}
 
 	return tokenString, err
+}
+
+func WithJWTAuth(handlerFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := getTokenFromRequest(r)
+
+		// Validate token
+		token, err := validateToken(tokenString)
+		if err != nil {
+			log.Printf("Failed to validate token: %v", err)
+			permissionDenied(w)
+			return
+		}
+		if !token.Valid {
+			log.Println("Invalid token")
+			permissionDenied(w)
+			return
+		}
+
+		// if is we need to feth the userID from the DB (id from token)
+		claims := token.Claims.(jwt.MapClaims)
+		str := claims["userID"].(string)
+
+		userID, _ := strconv.Atoi(str)
+
+		u, err := store.GetUserByID(userID)
+		if err != nil {
+			log.Printf("Failed to get user by id : %v", err)
+		}
+
+		// set context "userID" to the user ID
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, UserKey, u.ID)
+		r = r.WithContext(ctx)
+		handlerFunc(w, r)
+	}
+}
+
+func getTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	return ""
+}
+
+func validateToken(t string) (*jwt.Token, error) {
+	return jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method : %v", t.Header["alg"])
+		}
+		return []byte(config.Envs.JWTSecret), nil
+	})
+
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	utils.WriteError(w, http.StatusForbidden, fmt.Errorf("Permission Denied"))
+}
+
+func GetUserIDFromContext(ctx context.Context) int {
+	userID, ok := ctx.Value(UserKey).(int)
+	if !ok {
+		return -1
+	}
+	return userID
 }
